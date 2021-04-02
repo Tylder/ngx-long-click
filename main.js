be0768ac479fbb7bd8784ba2ae1be1a8ecffd19d
lofgrendaniel@hotmail.com <AUqjGN%6zG0$>
2021-04-02 11:54:59 +0900

    Auto-generated commit

---
+++ main.js
@@ -0,0 +1,255 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["main"],{
+
+/***/ 0:
+/*!*****************************************!*\
+  !*** multi ./projects/demo/src/main.ts ***!
+  \*****************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! C:\Programming\web\ng\libraries\ngx-long-click\projects\demo\src\main.ts */"rzic");
+
+
+/***/ }),
+
+/***/ "5d/s":
+/*!******************************************************************!*\
+  !*** ./projects/ngx-long-click/src/lib/ngx-long-click.module.ts ***!
+  \******************************************************************/
+/*! exports provided: NgxLongClickModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "NgxLongClickModule", function() { return NgxLongClickModule; });
+/* harmony import */ var _long_click_directive__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./long-click.directive */ "kt2x");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "fXoL");
+
+
+class NgxLongClickModule {
+}
+NgxLongClickModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵdefineNgModule"]({ type: NgxLongClickModule });
+NgxLongClickModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵdefineInjector"]({ factory: function NgxLongClickModule_Factory(t) { return new (t || NgxLongClickModule)(); }, imports: [[]] });
+(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_1__["ɵɵsetNgModuleScope"](NgxLongClickModule, { declarations: [_long_click_directive__WEBPACK_IMPORTED_MODULE_0__["LongClickDirective"]], exports: [_long_click_directive__WEBPACK_IMPORTED_MODULE_0__["LongClickDirective"]] }); })();
+
+
+/***/ }),
+
+/***/ "H5HQ":
+/*!*********************************************!*\
+  !*** ./projects/demo/src/app/app.module.ts ***!
+  \*********************************************/
+/*! exports provided: AppModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppModule", function() { return AppModule; });
+/* harmony import */ var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/platform-browser */ "jhN1");
+/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./app.component */ "NO+I");
+/* harmony import */ var _ngx_long_click_src_lib_ngx_long_click_module__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../ngx-long-click/src/lib/ngx-long-click.module */ "5d/s");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/core */ "fXoL");
+
+
+
+
+class AppModule {
+}
+AppModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵdefineNgModule"]({ type: AppModule, bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_1__["AppComponent"]] });
+AppModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵdefineInjector"]({ factory: function AppModule_Factory(t) { return new (t || AppModule)(); }, providers: [], imports: [[
+            _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"],
+            _ngx_long_click_src_lib_ngx_long_click_module__WEBPACK_IMPORTED_MODULE_2__["NgxLongClickModule"]
+        ]] });
+(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵsetNgModuleScope"](AppModule, { declarations: [_app_component__WEBPACK_IMPORTED_MODULE_1__["AppComponent"]], imports: [_angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"],
+        _ngx_long_click_src_lib_ngx_long_click_module__WEBPACK_IMPORTED_MODULE_2__["NgxLongClickModule"]] }); })();
+
+
+/***/ }),
+
+/***/ "NO+I":
+/*!************************************************!*\
+  !*** ./projects/demo/src/app/app.component.ts ***!
+  \************************************************/
+/*! exports provided: AppComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppComponent", function() { return AppComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "fXoL");
+/* harmony import */ var _ngx_long_click_src_lib_long_click_directive__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../../ngx-long-click/src/lib/long-click.directive */ "kt2x");
+
+
+const _c0 = function (a0) { return { background: a0 }; };
+class AppComponent {
+    constructor() {
+        this.colors = ['#7FFFD4', '#ff693d', '#DEB887', '#008B8B', '#8FBC8F'];
+        this.btn1ColorIndex = 0;
+        this.btn2ColorIndex = 0;
+        this.btn3ColorIndex = 0;
+    }
+    onLongClickReleaseBtn1(event) {
+        this.btn1ColorIndex += 1;
+        if (this.btn1ColorIndex >= this.colors.length) { // cycle round
+            this.btn1ColorIndex = 0;
+        }
+    }
+    onLongClickReleaseBtn2(event) {
+        this.btn2ColorIndex += 1;
+        if (this.btn2ColorIndex >= this.colors.length) { // cycle round
+            this.btn2ColorIndex = 0;
+        }
+    }
+    onNormalClick(event) {
+        this.btn3ColorIndex += 1;
+        console.log(this.btn3ColorIndex);
+        if (this.btn3ColorIndex >= this.colors.length) { // cycle round
+            this.btn3ColorIndex = 0;
+        }
+    }
+}
+AppComponent.ɵfac = function AppComponent_Factory(t) { return new (t || AppComponent)(); };
+AppComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({ type: AppComponent, selectors: [["app-root"]], decls: 6, vars: 13, consts: [[3, "longClick"], [3, "clickDelayMs", "longClick"], [3, "click"]], template: function AppComponent_Template(rf, ctx) { if (rf & 1) {
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](0, "button", 0);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵlistener"]("longClick", function AppComponent_Template_button_longClick_0_listener($event) { return ctx.onLongClickReleaseBtn1($event); });
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](1, "Press Me at least 0.5s");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](2, "button", 1);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵlistener"]("longClick", function AppComponent_Template_button_longClick_2_listener($event) { return ctx.onLongClickReleaseBtn2($event); });
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](3, "Press Me at least 1s");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](4, "button", 2);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵlistener"]("click", function AppComponent_Template_button_click_4_listener($event) { return ctx.onNormalClick($event); });
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵtext"](5, "Normal Button");
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+    } if (rf & 2) {
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵstyleMap"](_angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵpureFunction1"](7, _c0, ctx.colors[ctx.btn1ColorIndex]));
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵadvance"](2);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵstyleMap"](_angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵpureFunction1"](9, _c0, ctx.colors[ctx.btn2ColorIndex]));
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵproperty"]("clickDelayMs", 1000);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵadvance"](2);
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵstyleMap"](_angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵpureFunction1"](11, _c0, ctx.colors[ctx.btn3ColorIndex]));
+    } }, directives: [_ngx_long_click_src_lib_long_click_directive__WEBPACK_IMPORTED_MODULE_1__["LongClickDirective"]], styles: ["button[_ngcontent-%COMP%] {\n  padding: 10px;\n  margin-right: 10px;\n}\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uXFwuLlxcLi5cXC4uXFxhcHAuY29tcG9uZW50LnNjc3MiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7RUFDRSxhQUFBO0VBQ0Esa0JBQUE7QUFDRiIsImZpbGUiOiJhcHAuY29tcG9uZW50LnNjc3MiLCJzb3VyY2VzQ29udGVudCI6WyJidXR0b24ge1xyXG4gIHBhZGRpbmc6IDEwcHg7XHJcbiAgbWFyZ2luLXJpZ2h0OiAxMHB4O1xyXG59XHJcbiJdfQ== */"] });
+
+
+/***/ }),
+
+/***/ "ZcuK":
+/*!*******************************************************!*\
+  !*** ./projects/demo/src/environments/environment.ts ***!
+  \*******************************************************/
+/*! exports provided: environment */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "environment", function() { return environment; });
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+const environment = {
+    production: false
+};
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
+/***/ }),
+
+/***/ "kt2x":
+/*!*****************************************************************!*\
+  !*** ./projects/ngx-long-click/src/lib/long-click.directive.ts ***!
+  \*****************************************************************/
+/*! exports provided: LongClickDirective */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "LongClickDirective", function() { return LongClickDirective; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "fXoL");
+/* harmony import */ var rxjs__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! rxjs */ "qCKp");
+/* harmony import */ var rxjs_operators__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! rxjs/operators */ "kU1M");
+
+
+
+
+class LongClickDirective {
+    constructor(hostElement) {
+        this.hostElement = hostElement;
+        this.clickDelayMs = 500;
+        this.longClick = new _angular_core__WEBPACK_IMPORTED_MODULE_0__["EventEmitter"]();
+    }
+    ngAfterViewInit() {
+        this.mouseDown$ = Object(rxjs__WEBPACK_IMPORTED_MODULE_1__["fromEvent"])(this.hostElement.nativeElement, 'mousedown');
+        this.mouseUp$ = Object(rxjs__WEBPACK_IMPORTED_MODULE_1__["fromEvent"])(this.hostElement.nativeElement, 'mouseup');
+        this.touchStart$ = Object(rxjs__WEBPACK_IMPORTED_MODULE_1__["fromEvent"])(this.hostElement.nativeElement, 'touchstart');
+        this.touchEnd$ = Object(rxjs__WEBPACK_IMPORTED_MODULE_1__["fromEvent"])(this.hostElement.nativeElement, 'touchend');
+        Object(rxjs__WEBPACK_IMPORTED_MODULE_1__["merge"])(this.mouseDown$, this.touchStart$).pipe(
+        // tap(() => console.log('DOWN')),
+        // tap((val) => console.log(val)),
+        Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_2__["delay"])(this.clickDelayMs), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_2__["takeUntil"])(Object(rxjs__WEBPACK_IMPORTED_MODULE_1__["merge"])(this.mouseUp$, this.touchEnd$)), Object(rxjs_operators__WEBPACK_IMPORTED_MODULE_2__["repeat"])()).subscribe(val => this.longClick.emit(val));
+    }
+}
+LongClickDirective.ɵfac = function LongClickDirective_Factory(t) { return new (t || LongClickDirective)(_angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdirectiveInject"](_angular_core__WEBPACK_IMPORTED_MODULE_0__["ElementRef"])); };
+LongClickDirective.ɵdir = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineDirective"]({ type: LongClickDirective, selectors: [["", "longClick", ""]], inputs: { clickDelayMs: "clickDelayMs" }, outputs: { longClick: "longClick" } });
+
+
+/***/ }),
+
+/***/ "rzic":
+/*!***********************************!*\
+  !*** ./projects/demo/src/main.ts ***!
+  \***********************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/platform-browser */ "jhN1");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "fXoL");
+/* harmony import */ var _app_app_module__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app/app.module */ "H5HQ");
+/* harmony import */ var _environments_environment__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./environments/environment */ "ZcuK");
+
+
+
+
+if (_environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].production) {
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["enableProdMode"])();
+}
+_angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["platformBrowser"]().bootstrapModule(_app_app_module__WEBPACK_IMPORTED_MODULE_2__["AppModule"])
+    .catch(err => console.error(err));
+
+
+/***/ }),
+
+/***/ "zn8P":
+/*!******************************************************!*\
+  !*** ./$$_lazy_route_resource lazy namespace object ***!
+  \******************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncaught exception popping up in devtools
+	return Promise.resolve().then(function() {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "zn8P";
+
+/***/ })
+
+},[[0,"runtime","vendor"]]]);
+//# sourceMappingURL=main.js.map
